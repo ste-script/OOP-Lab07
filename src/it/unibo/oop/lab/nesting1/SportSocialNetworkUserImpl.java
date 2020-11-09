bf7a986d5ca6fccac6fb71bff0f5614e39e64224
Babini Stefano <stefano.babini5@studio.unibo.it>
2020-11-09 16:33:25 +0100

    Complete nesting 1

--- src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
@@ -53,12 +53,12 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      * TODO: initialize properly these sports
      */
     static {
-        SOCCER = null;
-        F1 = null;
-        MOTOGP = null;
-        VOLLEY = null;
-        BASKET = null;
-        BIKE = null;
+        SOCCER = new Sport("Soccer");
+        F1 = new Sport("F1");
+        MOTOGP = new Sport("Moto GP");
+        VOLLEY = new Sport("Volley");
+        BASKET = new Sport("Basket");
+        BIKE = new Sport("Bike");
     }
 
     /**
@@ -114,7 +114,7 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      */
     // TODO
     public void addSport(final Sport sport) {
-
+        this.sports.add(sport);
     }
 
     /**
@@ -126,7 +126,7 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
      */
     // TODO
     public boolean hasSport(final Sport s) {
-        return false;
+        return this.sports.contains(s);
     }
 
     /*
@@ -142,9 +142,19 @@ public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUse
          * Redefine equals so that two sports are equal only if they feature the
          * very same name. Remember that you must also redefine hashCode()!
          */
-        @Override
-        public boolean equals(final Object o) {
-            return false;
+        private final String name;
+        
+        public Sport(String name) {
+            this.name = name;
         }
+        @Override
+        public boolean equals(final Object s) {
+            return super.equals(s);
+        }
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+        
     }
 }
